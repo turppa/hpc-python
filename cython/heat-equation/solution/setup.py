bce75a24c6efceebded5e10eaac48d92f21b6f4b
etemmi <emmi.turppa@vtt.fi>
2020-07-27 17:07:06 +0300

    added numpy.get_include

--- cython/heat-equation/solution/setup.py
+++ cython/heat-equation/solution/setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
+import numpy
 
 setup(
      ext_modules=cythonize("heat.pyx"),
+     include_dirs=[numpy.get_include()]
 )
